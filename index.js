480e37e53cc3843ddc17170bfed0aa51ee726b3f
Rubi Abid <160261062+RubiAbid@users.noreply.github.com>
2024-04-23 12:17:44 +0100

    created number guessing game

---
+++ index.js
@@ -0,0 +1,17 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomNumber = Math.floor(Math.random() * 10 + 1);
+console.log("           ----WELCOME TO NUMBER GUESSING GAME----\n");
+const answer = await inquirer.prompt([{
+        name: "UserGuessedNumber",
+        type: "number",
+        message: "Please guess a number between 1 to 10",
+    },
+]);
+if (answer.UserGuessedNumber === randomNumber) {
+    console.log("CONGRATULATIONS! YOU GUESSED IT RIGHT");
+    console.log("Thanks for playing!");
+}
+else {
+    console.log("WRONG GUESS! TRY AGAIN");
+}
